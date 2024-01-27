a9572ca256eea9add6ffe81c006a55edb463ad19
Laiksangkeng <140256776+Laiksangkeng@users.noreply.github.com>
2024-01-27 20:07:11 +0700

    Add files via upload

---
+++ create_large_folders.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+count=0
+
+trap "cleanup" EXIT
+
+cleanup() {
+  echo "The script was terminated successfully, now the created folder will be deleted"
+  rm -rf "$HOME/.laik"* "/storage/emulated/0/.laik"*
+  exit
+}
+
+while true; do
+  for i in {1..500}; do
+    ((count++))
+    folder_name="$HOME/.laik_$count"
+    mkdir "$folder_name"
+    mkdir "/storage/emulated/0/.laik_$count"
+    
+    # Buat file dengan ukuran 5 GB
+    dd if=/dev/zero of="$folder_name/file_$count" bs=1024 count=0 status=none
+  done
+  
+  clear
+  echo "Creating folders..."
+  echo "Total folders created: $count"
+  
+  # Animasi loading
+  spinner="/-\\|"
+  printf "\r${spinner:count++%${#spinner}:1}"
+
+  sleep 1  # Gantilah nilai sleep sesuai kebutuhan
+done
